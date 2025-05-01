ace23d50fe89b2f5459fcc4cea917039ee4756a0
Xero1357 <emir90@live.nl>
2025-05-01 15:18:49 +0200

    Update App.jsx with latest changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
 import Homepage from './pages/Homepage';
 import NotFound from './pages/NotFound';
 
 import AddLinkForm from './components/collection/AddLinkForm'; //added
 import EditLinkForm from './components/collection/EditLinkForm'; //added
 
+import { Container, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className='d-flex flex-column min-vh-100'>
-        <Header />
-        <main className='flex-grow-1 d-flex flex-column'>
-          <Container fluid='md' className='py-4'>
-            <Routes>
-              <Route path='/' element={<Homepage />} />
-              
-              {/* ✅ New route for adding a link to a collection */}
-              <Route path='/collection/:collectionId/add-link' element={<AddLinkForm />} />
+    <main>
+      <Container fluid='md' className='py-4'>
+        <Routes>
+          {/* Home route */}
+          <Route path='/' element={<Homepage />} />
 
-              {/* ✅ New route for editing a link */}
-              <Route path='/link/:linkId/edit' element={<EditLinkForm />} />
+          {/* Route for adding a link to a collection */}
+          <Route
+            path='/collection/:collectionId/add-link'
+            element={<AddLinkForm />}
+          />
 
-              {/* Catch-all route for 404 (Not Found) */}
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </Container>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+          {/* Route for editing a link */}
+          <Route path='/link/:linkId/edit' element={<EditLinkForm />} />
+
+          {/* Catch-all route for 404 (Not Found) */}
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </Container>
+    </main>
   );
 }
 
 export default App;
-
-// 1
