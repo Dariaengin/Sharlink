c0dc7bfc51b0387165f5a3f113cc4768435c3bc3
Umka-dev <design.tvin@gmail.com>
2025-05-01 23:07:16 +0200

    add link controller and routs

--- backend/config/apiRoutes.js
+++ backend/config/apiRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('../controller/userController');
 const collectionController = require('../controller/collectionController');
+const linkController = require('../controller/linkController');
 
 const userAuth = require('../auth/auth');
 const router = express.Router();
@@ -14,5 +15,12 @@ router.get('/user', userAuth.isLoggedIn, userController.userPage);
 // Collection routes
 router.get('/collections', collectionController.getAllCollections);
 router.post('/collections/seed', collectionController.seedCollections);
+
+// Link routes
+router.post('/links', linkController.createLink);
+router.get('/links', linkController.getAllLinks);
+router.get('/links/:linkId', linkController.getLinkById);
+router.put('/links/:linkId', linkController.updateLink);
+router.delete('/links/:linkId', linkController.deleteLink);
 
 module.exports = router;
