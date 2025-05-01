e042899cbef752161ab6cfd9899edf3a2d95ce48
Xero1357 <emir90@live.nl>
2025-05-01 15:58:49 +0200

    Update App.jsx with latest changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
-import Homepage from './pages/Homepage';
-import ProfilePage from './pages/ProfilePage'; 
-import CollectionPage from './pages/CollectionPage';
-import CollectionsListPage from './pages/CollectionsListPage';
-import NotFound from './pages/NotFound';
-
-import AddLinkForm from './components/collection/AddLinkForm'; // added
-import EditLinkForm from './components/collection/EditLinkForm'; // added
-
-import './App.css';
-
-function App() {
-  return (
-    <Router>
-      <div className='d-flex flex-column min-vh-100'>
-        <Header />
-        <main className='flex-grow-1 d-flex flex-column'>
-          <Container fluid='md' className='py-4'>
-            <Routes>
-              <Route path='/' element={<Homepage />} />
               {/* Other routes here */}
               <Route path="/profile" element={<ProfilePage />} />
               <Route path="/collection/:collectionId" element={<CollectionPage />} />
@@ -34,13 +9,3 @@ function App() {
 
               {/* Catch-all route for 404 (Not Found) */}
               <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </Container>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
-
-export default App;
